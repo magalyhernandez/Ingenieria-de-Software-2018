2b84bb19e1dc2629069d4975a7cd9d7e86c7b6b7
Magaly Hernández <37159985+magalyhernandez@users.noreply.github.com>
2018-10-11 19:03:40 -0600

    Add files via upload

---
+++ Patrones/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+	<head>
+		<?php include "secciones/head.php"?>
+	</head>
+	<body>
+			<?php $menu="secciones/menu.php" ?>
+		<?php include "secciones/header.php" ?>
+		<?php include "secciones/patrones.php" ?>
+		<?php include "secciones/codigo.php" ?>
+		<?php  include "secciones/informacion.php"?>
+			<?php  include "secciones/wikis.php"?>
+
+			<?php include "secciones/footer.php"?>
+				<?php include "secciones/modalsPatrones.php" ?>
+				<?php include "secciones/modalsCodigo.php" ?>
+				<?php include "secciones/scripts.php"?>
+	</body>
+</html>
